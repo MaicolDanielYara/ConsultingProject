d2f074fbfb48ef34b8f19053bc34839b1a6f1d07
MaicolDanielYara <myarag17@gmail.com>
2021-04-08 13:20:16 -0500

    Solución de Errores y Diseño

--- src/ConsultingProject.Web/Controllers/HomeController.cs
+++ src/ConsultingProject.Web/Controllers/HomeController.cs
@@ -41,6 +41,10 @@ namespace ConsultingProject.Web.Controllers
         {
             return View();
         }
+        public IActionResult CitaConsultoria()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
